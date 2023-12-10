3fb63cd78fd2f0a9ef3a8b01351183f273a358ef
frontendsolutionsdev <frontendsolutionsdev@gmail.com>
2023-12-10 13:48:32 +0000

    JS
    js total price

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 window.addEventListener("resize", updateDots);
+
+
+
+
+
+function calculateTotal()
+{
+  let unit_price={
+    d160: 3,  
+  };
+
+  let item_price={}
+  
+  item_price.d160 = ($("#qty_d160").val() * unit_price.d160)
+  $("#price_d160").val(item_price.d160);
+ 
+  let total = item_price.d160;
+
+ 
+  $("#total_value").text(total);
+  
+}
+
+$(function()
+ {
+    $(".qty").on("change keyup",calculateTotal)
+})
+
